b5f9f6b16e5e17871fdec96dee36516559fecbd4
Nico <nicoarocena96@gmail.com>
2026-09-23 12:12:08 -0300

    1

--- components/nav/Nav.tsx
+++ components/nav/Nav.tsx
@@ -10,8 +10,8 @@ import Image from "next/image";
 // siguiente en aparecer.
 const LINKS = [
   { href: "#compania", id: "compania", label: "Compañía" },
-  { href: "#quimicos", id: "quimicos", label: "Químicos" },
   { href: "#papel", id: "papel", label: "Papel" },
+  { href: "#quimicos", id: "quimicos", label: "Químicos" },
   { href: "#soluciones", id: "soluciones", label: "Soluciones" },
   { href: "#planta", id: "planta", label: "Planta" },
 ];
